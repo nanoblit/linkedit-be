80e2426f53428c44745385ce409e970b28de065c
nanoblit <jakub.k.maleta@gmail.com>
2019-12-01 11:52:01 +0100

    Add internal server error message to error handler

--- src/auth-routes/registerRoute.js
+++ src/auth-routes/registerRoute.js
@@ -9,7 +9,7 @@ router.post("/register", async (req, res, next) => {
     if (!user.username || !user.password) {
       res.status(400).json({ error: "Username and password are required" });
     } else if (await db.getUserByName(user.username)) {
-      res.status(400).json({ error: "Username must be unique" });
+      res.status(400).json({ error: "A user with this username already exists" });
     } else {
       user.password = await bcrypt.hash(user.password, 12);
       const returnedUser = await db.addUser(user.username, user.password);
--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,6 +1,7 @@
 module.exports = (err, req, res, next) => {
   console.error("ERROR:", err);
   res.status(500).json({
+    error: "Internal server error",
     message: err.message,
     stack: err.stack
   });
